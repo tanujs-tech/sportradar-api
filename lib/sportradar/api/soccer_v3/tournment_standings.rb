e6609c7da9c4782322aaa4a3d36dcad72264d011
chanakyadevraj <cdevraj@grepruby.com>
2018-04-24 18:01:24 +0530

    added tournament standings

--- lib/sportradar/api/soccer_v3/tournment_standings.rb
+++ lib/sportradar/api/soccer_v3/tournment_standings.rb
@@ -4,16 +4,28 @@ module Sportradar
   module Api
     class SoccerV3::TournamentStandings < Data
       attr_accessor :response,
-                    :tournament,
+                    # :tournament,
                     :season,
                     :standings
 
       def initialize(data)
-        @response = data
-        @total = @response.detect { |a| a[:type] == 'total' }
-        @home = @response.detect { |a| a[:type] == 'home' }
-        @away = @response.detect { |a| a[:type] == 'away' }
-        @sport_events = parse_into_array(selector: standings, klass: Sportradar::Api::SoccerV3::SportEvent)
+        # @tournament = parse_into_array(selector: data[:tournament], klass: Sportradar::Api::SoccerV3::Tournament)
+        @season = parse_into_array(selector: data[:season], klass: Sportradar::Api::SoccerV3::Season)
+        # @response = data[:standings]
+        total_teams = data[:standings].detect { |a| a[:type] == 'total' } || {}
+        # TODO: when needed
+        # home = @response.detect { |a| a[:type] == 'home' }
+        # away = @response.detect { |a| a[:type] == 'away' }
+
+        @standings = parse_data(total_teams[:group])
+      end
+
+      private
+
+      def parse_data(total_teams)
+        total_teams.collect do |total_team|
+          parse_into_array(selector: total_team, klass: Sportradar::Api::SoccerV3::GroupStanding)
+        end
       end
     end
   end
